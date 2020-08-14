f932c0d03da447af608cbe05b8dfbe4cbc519588
Daniel Gilford <dgilford@users.noreply.github.com>
2020-08-14 11:33:09 -0400

    updating setup with new distribution name

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
+#! /usr/bin/env python
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyPI",
+    name="tcpypi",
+    # TODO: Update every time the version updates
     version="1.3",
-    author="Daniel M. Gilford",
+    author="Daniel M. Gilford, PhD",
     author_email="daniel.gilford@rutgers.edu",
     description="pyPI: Tropical cyclone potential intensity calculations in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dgilford/pyPI",
+    download_url="https://github.com/darothen/pyrcel",
     packages=setuptools.find_packages(),
+    package_data={"pyrcel": ["data/sample_data.nc"]},
     license="MIT LICENSE",
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -20,4 +25,13 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
+    # TODO: Update install requirements and corresponding documentation
+    install_requires=[
+        "xarray==0.15.1",
+        "numba==0.48.0",
+        "numpy==1.18.1",
+    ],
 )
+
+
+
