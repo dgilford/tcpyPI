96fb60a0f7a57f25515d43045a46de909b5e2427
Ben Mares <services-git-throwaway1@tensorial.com>
2025-02-18 19:47:47 +0100

    Separate njit no-op decorator from conditional

--- src/tcpyPI/numba.py
+++ src/tcpyPI/numba.py
@@ -8,18 +8,26 @@ This is useful for coverage testing.
 import os
 from functools import wraps
 
-if os.getenv('DISABLE_NUMBA') == '1':
-    # Create a no-op decorator to use when numba is disabled
-    def njit(*args, **kwargs):
-        def decorator(func):
-            @wraps(func)
-            def wrapper(*args, **kwargs):
-                return func(*args, **kwargs)
-            return wrapper
-        # Handle both @njit and @njit() syntax
-        if len(args) == 1 and callable(args[0]):
-            return decorator(args[0])
-        return decorator
+
+def noop_njit(*args, **kwargs):
+    """No-op decorator to replace @njit when numba is disabled."""
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    # Handle both @njit and @njit() syntax
+    if len(args) == 1 and callable(args[0]):
+        return decorator(args[0])
+    return decorator
+
+
+if os.getenv("DISABLE_NUMBA") == "1":
+    njit = noop_njit
 else:
     import numba as nb
+
     njit = nb.njit
